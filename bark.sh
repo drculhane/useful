ddf396c6e93728ed9c2374772668814cbe202f09
drculhane <160938282+drculhane@users.noreply.github.com>
2025-11-12 07:22:02 -0500

    Enhance bark.sh with echo statements
    Added echo statements for clarity and organization during setup.

--- bark.sh
+++ bark.sh
@@ -1,17 +1,22 @@
 cd ~
+echo "Making directories"
 mkdir tinkering
 mkdir projects
 mkdir misc
 # get the chapel source
+echo "Getting chapel"
 curl -LO https://github.com/chapel-lang/chapel/releases/download/2.6.0/chapel-2.6.0.tar.gz
 tar xvf chapel-2.6.0.tar.gz
 # create the arkouda directory and clone the main
 cd projects
+echo "Getting arkouda"
 git clone https://github.com/drculhane/arkouda.git
+cd arkouda
 git remote add upstream https://github.com/Bears-R-Us/arkouda.git
 git config --global user.name drculhane
 git config --global user.email drculhane@users.noreply.github.com
 # get miniconda, but don't install it yet
+echo "Getting miniconda"
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 # get other tools I use a lot
 echo "Getting useful tools."
@@ -24,11 +29,14 @@ mv chhunt.sh ~/misc
 mv pyhunt.sh ~/misc
 mv vimrc ~/.vimrc
 mv -r vimstuff ~/.vim
+echo "installing miniconda"
 # install miniconda
 cd
 sh miniconda.sh
 # now create the virtual environment
-cd $ARKOUDA_HOME
+echo "Creating environment"
+cd ~/projects/arkouda
 conda env create --file=arkouda-env-dev.yml
+conda activate arkouda-dev
 pip install -e . --no-deps
 echo "Time to close this shell and make a new one."
