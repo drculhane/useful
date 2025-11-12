1cbd2b1ba7af1ce30bf97d7c9221e6fa231ee678
drculhane <160938282+drculhane@users.noreply.github.com>
2025-11-12 08:02:04 -0500

    Enhance byte.sh with installation instructions
    Added installation steps for dependencies and Chapel.

--- byte.sh
+++ byte.sh
@@ -1,9 +1,18 @@
+echo "Installing dependencies"
+cd projects/arkouda
+pip install -e . --no-deps
 # install chapel prerequisites.  This will vary per Linux distro.  These are for Alma.
+echo "Getting chapel prerequisites."
 sudo dnf upgrade
 sudo dnf install gcc gcc-c++ m4 perl python3 python3-devel bash make gawk git cmake
 sudo dnf install which diffutils
 sudo dnf install llvm-devel clang clang-devel
+# get the chapel source
+echo "Getting chapel"
+curl -LO https://github.com/chapel-lang/chapel/releases/download/2.6.0/chapel-2.6.0.tar.gz
+tar xvf chapel-2.6.0.tar.gz
 # make chapel
+echo "Making chapel, etc."
 cd $CHPL_HOME
 make
 # do the rest
@@ -15,6 +24,7 @@ pip install -e . --no-deps
 cd $CHPL_HOME
 make chpldoc
 # now make arkouda
+echo "Making arkouda"
 cd $ARKOUDA_HOME
 make register-commands
 make
