b4de32cc02ae6df4562359d2e4cc8860c39f1722
drculhane <160938282+drculhane@users.noreply.github.com>
2025-11-21 07:48:14 -0500

    Delete interim.sh
    Redoing how this works.

--- interim.sh
+++
@@ -1,5 +0,0 @@
-echo "Creating arkouda-dev environment"
-cd projects/arkouda
-conda env create --file=arkouda-env-dev.yml
-pip install -e . --no-deps
-echo "Now exit this shell and start a new one."
